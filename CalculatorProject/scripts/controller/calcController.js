f8458569124e0e56b96f7bb8052bf62d4342d590
Gustavo Almansa <gustavosalmansa@gmail.com>
2024-10-18 18:13:10 -0300

    Addying querySelector to project

--- CalculatorProject/scripts/controller/calcController.js
+++ CalculatorProject/scripts/controller/calcController.js
@@ -1,6 +1,40 @@
 class CalcController {
     
     constructor(parameters) {
-        
+        this.displayCalc = "0";
+        this.currentDate = new Date();
+        this.initialize;
+ 
+        this.initialize();
     }
+    initialize(){
+        let DisplayCalcEl = document.querySelector("#display");
+        let dataEl = document.querySelector("data");
+        let timeEl = document.querySelector("hora");
+
+        DisplayCalcEl.innerHTML = "4567";
+        dataEl.innerHTML = "15/08/2024";
+        timeEl.innerHTML = "00.00";
+
+    }
+
+    get displayCalc(){
+        return this._displayCalc;
+    }
+
+    set displayCalc(value){
+        this._displayCalc = value;
+    }
+
+    get currentDate(){
+        return this._currentDate;
+    }
+
+    set currentDate(value){
+        this._currentDate = value;
+    }
+
+
+    
+
 }
